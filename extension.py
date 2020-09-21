e5afe7bd250c0652c1b8b00f59ba5b226910fd96
Aadit <66309635+Aadit017@users.noreply.github.com>
2020-09-21 12:30:53 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,3 +1,5 @@
 file=input("file name")
 f=file.split(".")
-print("The extension is= {}".format(f[1]))
+#print("The extension is= {}".format(f[1]))
+# use f strings over here
+print(f"the extension is {f}")
